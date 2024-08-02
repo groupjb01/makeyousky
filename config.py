91ed515c2ae2db1993754fe8dc62f3b2b1e91c61
groupjb01 <56146430+groupjb01@users.noreply.github.com>
2024-08-03 00:20:26 +0900

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# 설정 및 상수
-DATA_FILE_PATH = 'data_new_ver1_240725.xlsx'
-UNIVERSITY_RANGES_FILE_PATH = 'university_ranges.json'
-MAJOR_DATA_FILE_PATH = 'major_new.json'
-FILTER_DATA_FILE_PATH = 'filter_2nd.json'
